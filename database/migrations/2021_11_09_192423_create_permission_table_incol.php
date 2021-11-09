ffc0cc8cdf11598cfe2f6fddfecf8d09e2043cb4
devzone-pk <65868600+devzone-pk@users.noreply.github.com>
2021-11-10 00:25:55 +0500

    Add files via upload

---
+++ database/migrations/2021_11_09_192423_create_permission_table_incol.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionTableIncol extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->string('description')->nullable();
+            $table->string('portal',50)->nullable();
+            $table->string('section',50)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            //
+        });
+    }
+}
